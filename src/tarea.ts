3db72a5c39764dbd560501d4e8b56ac278f0a6f7
Arturo Molina <134606360+smallPingu@users.noreply.github.com>
2023-12-26 14:11:18 +0100

    Update tarea.ts
    Para resolver @AlvaroSanPal95/AssignedOrganization#7, vamos a crear una variable llamada Urgencia que será de tipo urgencia, un enumerado que crearemos.

--- src/tarea.ts
+++ src/tarea.ts
@@ -1,5 +1,14 @@
-interface Tarea {
+enum Urgencia {
+  Baja,
+  Media,
+  Alta,
+  MuyAlta
+}
+
+interface Tarea 
+{     
   titulo: string;
   descripcion: string;
   duracion: number;
+  urgencia: Urgencia; 
 }
